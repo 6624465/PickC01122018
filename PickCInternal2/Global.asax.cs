a6acd9eb7f730a4f19c5b9222ca797bacb2d1149
vijay <vijay@sulabs.tech>
2018-02-15 19:53:36 +0530

    MyDateTimeModelBinder Added

--- PickCInternal2/Global.asax.cs
+++ PickCInternal2/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PickCInternal2.HtmlHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,9 @@ namespace PickCInternal2
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ModelBinders.Binders.Add(typeof(DateTime), new MyDateTimeModelBinder());
+            ModelBinders.Binders.Add(typeof(DateTime?), new MyDateTimeModelBinder());
         }
     }
 }
