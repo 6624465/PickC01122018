209df1f2502997b037d836cb5b714e4fcffa2b3f
vijay <vijay@logiconglobal.com>
2018-02-28 14:18:20 +0530

    tt test

--- PickCApi/Areas/Master/Controllers/AddressController.cs
+++ PickCApi/Areas/Master/Controllers/AddressController.cs
@@ -11,7 +11,9 @@ using Master.BusinessFactory;
 
 namespace PickCApi.Areas.Master.Controllers
 {
-        //vijay comments
+
+    //vijay comments
+    //https://stackoverflow.com/questions/48956927/cant-push-git-visual-studio-git-failed-with-a-fatal-error-fatal-httprequeste
     [RoutePrefix("api/master/address")]
     public class AddressController : ApiController
     {
